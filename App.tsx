27a2e54fddfbf63613b6c2253af0cf4845ac4fc2
Brandon Shen <83319404+BrandonS09@users.noreply.github.com>
2024-07-09 11:41:58 -0700

    intial google maps integration

--- App.tsx
+++ App.tsx
@@ -1,11 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import * as Location from "expo-location";
 
 export default function App() {
+  const [location, setLocation] = useState<Location.LocationObject | null>(null); // Specify the type explicitly
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let initialRegion = {
+    latitude: 37.4221,
+    longitude: -122.0853,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  };
+
+  if (location) {
+    initialRegion = {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.01,
+      longitudeDelta: 0.01,
+    };
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {location ? (
+        <MapView
+          provider={PROVIDER_GOOGLE} // Specify Google Maps as the provider
+          style={styles.map}
+          initialRegion={initialRegion}
+        />
+      ) : (
+        <Text>Loading...</Text>
+      )}
     </View>
   );
 }
@@ -13,8 +53,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  map: {
+    flex: 1,
+    width: "100%",
   },
 });
