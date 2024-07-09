fe4691fc6b7a856034fde2b85d316c5aacafece3
Brandon Shen <83319404+BrandonS09@users.noreply.github.com>
2024-07-09 14:22:09 -0700

    lando

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 import * as Location from "expo-location";
+import uIcon from "../f1maps/assets/lando.png";
 
 export default function App() {
-  const [location, setLocation] = useState<Location.LocationObject | null>(
-    null
-  ); // Specify the type explicitly
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [heading, setHeading] = useState<number | null>(null); // Store heading direction
 
   useEffect(() => {
     (async () => {
@@ -16,8 +16,14 @@ export default function App() {
         return;
       }
 
+      // Get initial location
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
+
+      // Subscribe to heading updates
+      Location.watchHeadingAsync((headingData) => {
+        setHeading(headingData.trueHeading);
+      });
     })();
   }, []);
 
@@ -41,15 +47,21 @@ export default function App() {
     <View style={styles.container}>
       {location ? (
         <MapView
-          provider={PROVIDER_GOOGLE} // Specify Google Maps as the provider
+          provider={PROVIDER_GOOGLE}
           style={styles.map}
           initialRegion={initialRegion}
         >
           <Marker
-            coordinate={{ latitude: location.coords.latitude, longitude: location.coords.longitude }}
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
             title="Marker Title"
             description="Marker Description"
-          />
+            rotation={heading || 0} // Rotate marker based on heading, default to 0 if heading is null
+          >
+            <Image source={uIcon} style={{width: 50, height: 50}} resizeMode="contain"></Image>
+          </Marker>
         </MapView>
       ) : (
         <Text>Loading...</Text>
